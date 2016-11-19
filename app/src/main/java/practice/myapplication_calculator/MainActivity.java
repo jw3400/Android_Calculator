112947a2fde45df60b2f33975631c49588a25261
ChoiJinwon <jw3400@gmail.com>
2016-11-19 09:03:32 +0900

    간단한 Calculator작업 업그레이드

--- app/src/main/java/practice/myapplication_calculator/MainActivity.java
+++ app/src/main/java/practice/myapplication_calculator/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     Button b2;
     Button b3;
     Button b4;
+    Button b5;
     TextView label1;
 
 
@@ -26,32 +28,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         label1 = (TextView) findViewById(R.id.textView);
+        e1 = (EditText) findViewById(R.id.editText);
+        e2 = (EditText) findViewById(R.id.editText2);
 
         b1 = (Button) findViewById(R.id.button);
         b1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                e1 = (EditText) findViewById(R.id.editText);
-                e2 = (EditText) findViewById(R.id.editText2);
-                int input1 = Integer.parseInt(e1.getText().toString());
-                int input2 = Integer.parseInt(e2.getText().toString());
-                int result;
-                result = input1 + input2;
-                label1.setText("계산 결과는 : "+result+" ");
 
+                String input1 = e1.getText().toString();
+                String input2 = e2.getText().toString();
+                double result;
 
+                if (e1.getText().toString().equals("") || e2.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "숫자를 입력하지 않았습니다.", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    result = Double.parseDouble(input1) + Double.parseDouble(input2);
+                    label1.setText("계산 결과는 : "+result+" ");
+                }
             }
         });
 
         b2 = (Button) findViewById(R.id.button2);
         b2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                e1 = (EditText) findViewById(R.id.editText);
-                e2 = (EditText) findViewById(R.id.editText2);
-                int input1 = Integer.parseInt(e1.getText().toString());
-                int input2 = Integer.parseInt(e2.getText().toString());
-                int result;
-                result = input1 - input2;
-                label1.setText("계산 결과는 : "+result+" ");
+
+                String input1 = e1.getText().toString();
+                String input2 = e2.getText().toString();
+                double result;
+
+                if (e1.getText().toString().equals("") || e2.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "숫자를 입력하지 않았습니다.", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    result = Double.parseDouble(input1) - Double.parseDouble(input2);
+                    label1.setText("계산 결과는 : "+result+" ");
+                }
+
 
             }
         });
@@ -59,26 +72,63 @@ public class MainActivity extends AppCompatActivity {
         b3 = (Button) findViewById(R.id.button3);
         b3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                e1 = (EditText) findViewById(R.id.editText);
-                e2 = (EditText) findViewById(R.id.editText2);
-                int input1 = Integer.parseInt(e1.getText().toString());
-                int input2 = Integer.parseInt(e2.getText().toString());
-                int result;
-                result = input1 * input2;
-                label1.setText("계산 결과는 : "+result+" ");
+
+                String input1 = e1.getText().toString();
+                String input2 = e2.getText().toString();
+                double result;
+
+                if (e1.getText().toString().equals("") || e2.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "숫자를 입력하지 않았습니다.", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    result =  Double.parseDouble(input1) * Double.parseDouble(input2);
+                    label1.setText("계산 결과는 : " + result + " ");
+                }
             }
+
         });
 
         b4 = (Button) findViewById(R.id.button4);
         b4.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                e1 = (EditText) findViewById(R.id.editText);
-                e2 = (EditText) findViewById(R.id.editText2);
-                int input1 = Integer.parseInt(e1.getText().toString());
-                int input2 = Integer.parseInt(e2.getText().toString());
-                int result;
-                result = input1 / input2;
-                label1.setText("계산 결과는 : "+result+" ");
+
+                String input1 = e1.getText().toString();
+                String input2 = e2.getText().toString();
+                double result;
+
+                if (e1.getText().toString().equals("") || e2.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "숫자를 입력하지 않았습니다.", Toast.LENGTH_LONG).show();
+                }
+                else if (e2.getText().toString().equals("0")) {
+                    Toast.makeText(getApplicationContext(), "0으로 나눌 수 없습니다.", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    result = Double.parseDouble(input1) / Double.parseDouble(input2);
+                    label1.setText("계산 결과는 : " + result + " ");
+                }
+
+
+            }
+        });
+
+        b5 = (Button) findViewById(R.id.button5);
+        b5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                String input1 = e1.getText().toString();
+                String input2 = e2.getText().toString();
+                double result;
+
+                if (e1.getText().toString().equals("") || e2.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "숫자를 입력하지 않았습니다.", Toast.LENGTH_LONG).show();
+                }
+                else if (e2.getText().toString().equals("0")) {
+                    Toast.makeText(getApplicationContext(), "0으로 나머지를 못 구합니다.", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    result = Double.parseDouble(input1) % Double.parseDouble(input2);
+                    label1.setText("계산 결과는 : "+result+" ");
+                }
             }
         });
     }
